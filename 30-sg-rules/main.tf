78f9777e8ccbc39bf28d6f1d03ac0617c5b07b3d
balu <balu@gmail.com>
2026-06-02 17:53:24 +0530

    sg rules

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -18,5 +18,15 @@ resource "aws_security_group_rule" "bastion_to_laptop" {
   cidr_blocks = ["0.0.0.0/0"]
 }
 
+# Mongodb
+resource "aws_security_group_rule" "mongodb_to_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = local.mongodb_sg_id
+  source_security_group_id = local.bastion_sg_id
+}
+
 
 
