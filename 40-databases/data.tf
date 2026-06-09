892e9235e098b61e4591c1453033a1c8bdb9e4fe
balu <balu@gmail.com>
2026-06-09 12:34:50 +0530

    using terraform

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -30,6 +30,6 @@ data "aws_ssm_parameter" "redis_sg_id" {
   name = "/${var.project_name}/${var.environment}/redis_sg_id"
 }
 
-data "aws_ssm_parameter" "robbitmq_sg_id" {
-  name = "/${var.project_name}/${var.environment}/robbitmq_sg_id"
-}
+# data "aws_ssm_parameter" "robbitmq_sg_id" {
+#   name = "/${var.project_name}/${var.environment}/robbitmq_sg_id"
+# }
