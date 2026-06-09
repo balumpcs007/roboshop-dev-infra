7732826f430d97fc19d5bd120f534d2b9ea63e04
balu <balu@gmail.com>
2026-06-09 14:42:37 +0530

    using terraform

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -48,5 +48,15 @@ resource "aws_security_group_rule" "rabbitmq_to_bastion" {
   source_security_group_id = local.bastion_sg_id
 }
 
+# mysql
+resource "aws_security_group_rule" "mysql_to_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = local.mysql_sg_id
+  source_security_group_id = local.bastion_sg_id
+}
+
 
 
