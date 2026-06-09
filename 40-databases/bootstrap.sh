4a30de3b0e00a1ec4498d197bd1b7c751d33f586
balu <balu@gmail.com>
2026-06-09 16:27:03 +0530

    using terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -2,13 +2,8 @@
 
 component=$1
 environment=$2
-
-if [ -z "$component" ] || [ -z "$environment" ]; then
-  echo "ERROR: Usage: $0 <component> <environment>"
-  exit 1
-fi
-
 dnf install ansible -y
+# ansible-pull -U https://github.com/balumpcs007/ansible-roboshop-roles-tf.git -e component=$component main.yaml
 
 REPO_URL=https://github.com/balumpcs007/ansible-roboshop-roles-tf.git
 REPO_DIR=/opt/roboshop/ansible
@@ -16,15 +11,19 @@ ANSIBLE_DIR=ansible-roboshop-roles-tf
 
 mkdir -p $REPO_DIR
 mkdir -p /var/log/roboshop/
+touch ansible.log
 
-cd $REPO_DIR || exit 1
+cd $REPO_DIR
+
+# check if ansible repo is already cloned or not
 
 if [ -d $ANSIBLE_DIR ]; then
-  cd $ANSIBLE_DIR
-  git pull
+
+    cd $ANSIBLE_DIR
+    git pull
 else
-  git clone $REPO_URL
-  cd $ANSIBLE_DIR
+    git clone $REPO_URL
+    cd $ANSIBLE_DIR
 fi
 
-ansible-playbook -e component=$component -e env=$environment main.yaml | tee /var/log/roboshop/ansible.log
+ansible-playbook -e component=$component -e env=$environment main.yaml
