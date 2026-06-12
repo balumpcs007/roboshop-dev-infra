43d6a2fb07058d9f9d21530d43c4111fbd98988e
balu <balu@gmail.com>
2026-06-12 08:24:15 +0530

    27017 port opening

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -57,6 +57,26 @@ resource "aws_security_group_rule" "mysql_to_bastion" {
   security_group_id = local.mysql_sg_id
   source_security_group_id = local.bastion_sg_id
 }
+# catalogue
+resource "aws_security_group_rule" "catalogue_to_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = local.catalogue_sg_id
+  source_security_group_id = local.bastion_sg_id
+}
+
+resource "aws_security_group_rule" "mongodb_to_catalogue" {
+  type              = "ingress"
+  from_port         = 27017
+  to_port           = 27017
+  protocol          = "tcp"
+  security_group_id = local.mongodb_sg_id
+  source_security_group_id = local.catalogue_sg_id
+}
+
+
 
 
 
