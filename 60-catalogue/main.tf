fdb2b6cbb946699390767240b4e7916147e5581e
balu <balu@gmail.com>
2026-06-12 10:02:56 +0530

    using terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -40,3 +40,17 @@ resource "terraform_data" "catalogue" {
      ]
   }
 }
+
+# stop the instance to take image
+resource "aws_ec2_instance_state" "catalogue" {
+  instance_id = aws_instance.catalogue.id
+  state       = "stopped" # Change to "running" to start it back up
+  depends_on = [terraform_data.catalogue]
+}
+
+resource "aws_ami_from_instance" "catalogue" {
+  name               = "${local.common_name_suffix}-catalogue"
+  source_instance_id = "aws_instance.catalogue.id"
+  depends_on = [aws_ec2_instance_state.catalogue]
+}
+
