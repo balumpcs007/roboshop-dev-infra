5f805507dde0eecd11fdb0785cbe650860b717dd
balu <balu@gmail.com>
2026-06-15 16:01:21 +0530

    bastion

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# growing the /home volume for terraform purpose
+growpart /dev/nvme0n1 4
+lvextend -L +30G /dev/mapper/RootVG-homeVol
+xfs_growfs /home
+
+# sudo lvreduce -r -L 6G /dev/mapper/RootVG-rootVol
+
 sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo dnf makecache
 sudo dnf install -y terraform
