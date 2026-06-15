93cf0fda9a76c901948003ce8c6de46e0cd1c0fe
balu <balu@gmail.com>
2026-06-15 20:33:51 +0530

    using terraform

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -3,6 +3,7 @@ locals {
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
   private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
+  backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
   vpc_id = data.aws_ssm_parameter.vpc_id.value
   common_tags = {
         Project     = var.project_name
